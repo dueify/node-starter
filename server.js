004e980c7f46b5db4cf5ed944b02165497066552
Martin Due <lim0ff@gmail.com>
2019-04-14 22:45:06 +0200

    Formatting

--- server.js
+++ server.js
@@ -15,10 +15,17 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "10mb",
+    extended: false
+  })
+);
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
